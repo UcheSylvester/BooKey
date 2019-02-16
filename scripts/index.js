7cd1cb0777bdd925f867a075c3913db8e77956ca
uchenna <okorocode@gmail.com>
2019-02-16 18:08:42 +0100

    added a TODO

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+/*
+TODO :
+
+1. Write a function fetchBooks(),
+
+2. Store searchButton, booksAPI and inputSearch value in a variables: button, booksAPI 
+   and searchText respectively.
+
+3. fetch books from the API (with the API and searchText ) using fetch and promises
+
+4. Display the result in an unordered list with book title, book cover and link to further
+   information on the books' web site
+
+5. Listen for click event on the the searchButton and call fetchBooks function
+*/
