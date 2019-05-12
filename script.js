a63f707a5facc3c34eed788d77017f910a39f487
Uchenna <okorocode@gmail.com>
2019-05-12 08:11:29 +0100

    REFACTOR: removed initial searched results from the page

--- script.js
+++ script.js
@@ -27,18 +27,40 @@
 
                 if(books) {
                     console.log(books)
+                    // checking for initial search results and removing them
+                    if(responseContainer.hasChildNodes()) {
+                        responseContainer.firstElementChild.remove()
+                        displayBooks(books)
+                    } else {
+                        displayBooks(books)
+                    }
+
+                } else {
+                    const errorMessage =  `<p class="text-center">Sorry.... There is no books for <em>${searchedForText}</em>. Check back later</p>`;
+                    // checking for initial search results and removing them
+                    if(responseContainer.hasChildNodes()) {
+                        responseContainer.firstElementChild.remove()
+                        htmlContent = errorMessage
+                    } else {
+                        htmlContent = errorMessage;
+                    }
+                }
+
+                // TO DISPLAY BOOKS ON THE PAGE
+                function displayBooks(books) {
                     htmlContent = '<ul>' + books.map(book => 
-                        `<li>
+                        `<li class="card">
                             <h3><a href="${book.volumeInfo.previewLink}" target="_blank">${book.volumeInfo.title}</a></h3>
                             <img src="${book.volumeInfo.imageLinks.thumbnail}" alt="${searchedForText}">
                             <p>${book.volumeInfo.publishedDate}</p>                            
                             <p>${book.volumeInfo.authors}</p>
+                        
                         </li>`
                     ).join('') + '</ul>'
-
-                    responseContainer.insertAdjacentHTML('afterbegin', htmlContent)
-
                 }
+
+                responseContainer.insertAdjacentHTML('afterbegin', htmlContent)
+
 
             }
 
